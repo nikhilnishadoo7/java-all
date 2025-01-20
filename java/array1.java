12a1406b077b07df01003d656e360adf83d811c4
nikhilnishadoo7 <rrnrrn007@gmail.com>
2025-01-20 12:40:02 +0530

    first commit

---
+++ java/array1.java
@@ -0,0 +1,18 @@
+public class array1 {
+    public static void main(String[] args) {
+        // int a=(int) 7.7;
+        // System.out.println(a);
+
+        // byte f = 90;
+        // float v=f;
+        // System.out.println(v);
+        char b ='=';
+        int c = (int)b;
+        System.out.println(c);
+
+        char b ='=';
+        int c = (int)b;
+        System.out.println(c);
+
+    }
+}
---
+++ java/java1/stringp1.java
@@ -0,0 +1,29 @@
+package java1;
+
+import java.util.*;
+public class stringp1 {
+    public static void main(String[] args) {
+        
+        // String b;
+        Scanner sr =new Scanner(System.in);
+        String s= sr.nextLine();
+        int l=0;
+        int r= s.length();
+        // System.out.println(s);
+        // int n1= 9;
+        // int n2 = 10;
+        // System.out.println(n1<n2);
+        // System.out.println(n1>n2);
+        // System.out.println(n1<=n2);
+        // System.out.println(n1>=n2);
+        // System.out.println(n1==n2);
+        // System.out.println(n1!=n2);
+    //    while(l<r){
+    //     char temp = s[l];
+    //        s[l]= s[r];
+    //        s[r] = temp;
+    //    }
+     
+
+    }
+}
