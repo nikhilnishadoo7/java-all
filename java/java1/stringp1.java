220a854c96da4abffe9ad9a425a77ab3dcec497f
nikhilnishadoo7 <rrnrrn007@gmail.com>
2025-01-20 13:42:17 +0530

    all

--- java/java1/stringp1.java
+++ java/java1/stringp1.java
@@ -2,16 +2,20 @@ package java1;
 
 import java.util.*;
 public class stringp1 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         
         // String b;
-        Scanner sr =new Scanner(System.in);
-        String s= sr.nextLine();
-        int l=0;
-        int r= s.length();
+        // Scanner sr =new Scanner(System.in);
+        // String s= sr.nextLine();
+        // int l=0;
+        // int r= s.length();
         // System.out.println(s);
         // int n1= 9;
         // int n2 = 10;
+        // int n3=9;
         // System.out.println(n1<n2);
         // System.out.println(n1>n2);
         // System.out.println(n1<=n2);
@@ -23,7 +27,24 @@ public class stringp1 {
     //        s[l]= s[r];
     //        s[r] = temp;
     //    }
-     
-
+    //    System.out.println(s);  
+    // int i =1;
+    // System.out.println(i++);
+    // System.out.println(i--);
+    // System.out.println(i);
+    // System.out.println(++i);
+    // System.out.println(--i);
+    // System.out.println(i);
+    
+    float f = 10.5f;
+    char c = 'a';
+    boolean b = true;
+    String str = "Hello";
+    System.out.println(++f);
+    System.out.println(++c);
+    //  System.out.println(b++);
+    //  System.out.println(str++);
+    
+         
     }
 }
